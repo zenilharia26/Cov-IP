ffd50a51ccaf80ff43c9f9bea95f41e1aacaf8b6
arghyadeep99 <arghyadeep.d@somaiya.edu>
2021-02-01 11:21:12 +0530

    Added URL functionality to read full news

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ const countryToCode = {
 }
 
 $( document ).ready(function() {
-    const proxy = 'http://localhost:8080/';
-    const apiEndpoint = `${proxy}https://newsapi.org/v2/top-headlines?q=corona&language=en`;
-    const apiKey = '148162a1b18c4fb092bbf0fe2a2d2885';
+    const proxy = 'http://cors-anywhere.herokuapp.com/';
+    const apiEndpoint = `${proxy}https://newsapi.org/v2/top-headlines?q=covid&language=en&source=the-hindu`;
+    //const apiKey = '7fcff72941a749f596ca167c566fd14e'; //Arghyadeep
+    const apiKey = '148162a1b18c4fb092bbf0fe2a2d2885'; //Zenil
     let countryDropdown = document.getElementById('country');
 
     for(let country in countryToCode) {
@@ -28,7 +29,7 @@ $( document ).ready(function() {
 });
 
 function loadNews(news) {
-    // console.log(news['articles']);
+    console.log(news['articles']);
     newsList = document.getElementsByTagName("ul")[0];
     newsList.innerHTML = "";
 
@@ -44,6 +45,9 @@ function loadNews(news) {
             document.getElementById("news-heading").innerText = article['title'];
             document.getElementById("news-date").innerText = article['publishedAt'];
             document.getElementById("news-description").innerText = article['description'];
+            document.getElementById("news-url").innerHTML = "Read More";
+            document.getElementById("news-url").href = article['url'];
+            document.getElementById("news-url").target = "_blank";
         })
     }
 
